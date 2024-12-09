018c0967039756cbbbd2344c89619947a59715d8
alex <liuhlightning@qq.com>
2024-12-09 19:29:28 +0800

    update util

--- src/util/paramsToString.ts
+++ src/util/paramsToString.ts
@@ -2,20 +2,25 @@ export interface ParamsToStringParam {
   lowerCase?: boolean;
 }
 
-type Params = Record<string, string | number | boolean | null>;
+export type Params = Record<string, string | number | boolean | null>;
 
 /**
  * Parse the object to query string without encoding based on the ascii key order
  */
 export function paramsToString(data: Params, param: ParamsToStringParam = {}) {
+  // avoid undefined value
   const keys = Object.keys(data)
     .filter((key) => data[key] !== undefined)
     .sort();
+
+  // build a new params
   const res: Params = {};
-  keys.forEach((key) => {
+  for (const key of keys) {
     const tempKey = param.lowerCase ? key.toLowerCase() : key;
     res[tempKey] = data[key];
-  });
+  }
+
+  // build string
   const strList: string[] = [];
   for (const key in res) {
     strList.push(`${key}=${res[key]}`);
--- src/util/timestamp.ts
+++ src/util/timestamp.ts
@@ -1,6 +1,6 @@
 /**
  * 生成时间戳 秒单位
  */
-export function timestamp() {
-  return (new Date().getTime() / 1000.0).toFixed(0);
+export function timestamp(): string {
+  return (Date.now() / 1000.0).toFixed(0);
 }
