09d8edf2f1ae4cb51250b3f4cfc74718220e81b3
alex <liuhlightning@qq.com>
2024-12-09 10:54:17 +0800

    test

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,20 +1,43 @@
-import { strictEqual } from "node:assert";
+import assert, { notEqual, strictEqual } from "node:assert";
 import { describe, test } from "node:test";
 import { join } from "path";
 import { WxPay } from "../src";
 
 const wxPay = new WxPay({
-  appId: "",
+  appId: "test app id",
   apiv3Key: "",
   mchId: "1900009191",
   certSerial: "1DDE55AD98ED71D6EDD4A4A16996DE7B47773A8C",
   wxPayPublicKeyPath: join(process.cwd(), "cert", "1900009191_wxp_pub.pem"),
   privateKeyPath: join(process.cwd(), "cert", "test.pem"),
-  notifyUrl: "",
+  notifyUrl: "https://test-url",
 });
 
 // this will fail because we do not know the private key
 describe("wx pay unit test", () => {
+  test("constructor", () => {
+    strictEqual(wxPay.appId, "test app id");
+    strictEqual(wxPay.mchId, "1900009191");
+    strictEqual(wxPay.certSerial, "1DDE55AD98ED71D6EDD4A4A16996DE7B47773A8C");
+    strictEqual(wxPay.notifyUrl, "https://test-url");
+  });
+
+  test("timestamp", () => {
+    const t = wxPay.timestamp;
+    const n = Date.now();
+    assert(n - parseInt(t) * 1000.0 < 1000.0, "timestamp is not correct");
+  });
+
+  test("nonce str", () => {
+    const n = wxPay.nonceStr;
+    strictEqual(n.length, 16, "nonce str length is not correct");
+  });
+  test("nonce str", () => {
+    const n1 = wxPay.nonceStr;
+    const n2 = wxPay.nonceStr;
+    notEqual(n1, n2, "nonce str is not unique");
+  });
+
   test("sign", { skip: true }, () => {
     strictEqual(
       wxPay.buildAuthorization(
--- test/util.test.ts
+++ test/util.test.ts
@@ -1,16 +1,17 @@
-import { strictEqual } from "node:assert";
+import assert, { strictEqual, notEqual } from "node:assert";
 import { describe, test } from "node:test";
 import { normalizeUrl } from "../src/util/normalizeUrl";
 import { paramsToString } from "../src/util/paramsToString";
+import { timestamp } from "../src/util/timestamp";
+import { nonceStr } from "../src/util/nonceStr";
 
-describe("util test", () => {
+describe("util unit test", () => {
   test("delete url hash", () => {
     strictEqual(
       normalizeUrl("http://mp.weixin.qq.com?params=value#some-hash-value"),
       "http://mp.weixin.qq.com?params=value",
     );
   });
-
   test("delete url hash", () => {
     strictEqual(
       normalizeUrl("http://mp.weixin.qq.com/?params=value#some-hash-value"),
@@ -33,7 +34,6 @@ describe("util test", () => {
       "jsapi_ticket=sM4AOVdWfPE4DxkXGEs8VMCPGGVi4C3VM0P37wVUCFvkVAy_90u5h9nbSlYy3-Sl-HhTdfl2fzFy1AOcHKP7qg&noncestr=Wm3WZYTPz0wzccnW&timestamp=1414587457&url=http://mp.weixin.qq.com?params=value",
     );
   });
-
   test("param stringify", () => {
     strictEqual(
       paramsToString(
@@ -48,5 +48,21 @@ describe("util test", () => {
       ),
       "jsapi_ticket=sM4AOVdWfPE4DxkXGEs8VMCPGGVi4C3VM0P37wVUCFvkVAy_90u5h9nbSlYy3-Sl-HhTdfl2fzFy1AOcHKP7qg&noncestr=Wm3WZYTPz0wzccnW&timestamp=1414587457&url=http://mp.weixin.qq.com?params=value",
     );
+  });
+
+  test("timestamp", () => {
+    const t = timestamp();
+    const n = Date.now();
+    assert(n - parseInt(t) * 1000.0 < 1000.0, "timestamp is not correct");
+  });
+
+  test("nonce str", () => {
+    const n = nonceStr();
+    strictEqual(n.length, 16, "nonce str length is not correct");
+  });
+  test("nonce str", () => {
+    const n1 = nonceStr();
+    const n2 = nonceStr();
+    notEqual(n1, n2, "nonce str is not unique");
   });
 });
